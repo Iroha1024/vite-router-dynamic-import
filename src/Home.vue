ed67e118d9a74b1dd4f673be451db2845d5045ff
Iroha1024 <47475495+Iroha1024@users.noreply.github.com>
2021-02-07 11:30:48 +0800

    build error

---
+++ src/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>home</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { throttle } from "lodash-es";
+
+export default defineComponent({
+  setup() {
+    const fn = throttle(() => {}, 100);
+
+    return {
+      fn
+    }
+  },
+});
+</script>
+
+<style></style>
